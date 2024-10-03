45bf5791ca7d500d444bb9ac8a0dc079a4e63ce4
Keavan Farsad <keavanfarsad@gmail.com>
2024-10-03 18:39:01 -0400

    Lab 2 Files from CS5001

---
+++ functions.py
@@ -0,0 +1,11 @@
+'''
+    CS 5001 Lab 2
+    Fall 2020
+    File: functions.py
+'''
+
+def menu(message):
+    answer = input(message)
+    answer = answer.upper() # convert to uppercase for comparison
+    return answer
+
